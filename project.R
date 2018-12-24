5b75158c215b6115db10174526b17f67f06e6272
ibrahim <ibrahimka2018@gmail.com>
2018-12-24 05:53:12 +0200

    first commit

---
+++ project.R
@@ -0,0 +1,48 @@
+library(caret)
+# read train & test data
+training <- read.csv('C:/Users/ibrahim/Desktop/P8 week 4/pml-training.csv')
+testing  <- read.csv('C:/Users/ibrahim/Desktop/P8 week 4/pml-testing.csv')
+training <- training[c(2:160)]
+ColsNames <- names(training)
+lenCol = 19622
+count = 1
+for (colName in ColsNames) {
+  currentCol = training[colName]
+  currentCol = currentCol[[colName]]
+  lenNA   = length(subset(currentCol , is.na(currentCol)))
+  if(is.factor(currentCol)){
+    colAsStr <- as.character(currentCol)
+    lenSpace <- length(subset(colAsStr , colAsStr == ""))
+    lenNA = lenNA + lenSpace  
+  }
+  if(((lenNA / lenCol) * 100) < 30)
+  {
+    
+    if(!is.character(currentCol)){
+        if(exists('newTraining'))
+        {
+          colN[count] = colName
+          newTraining <- cbind(newTraining , new = currentCol)  
+        }
+        else
+        {
+          colN = colName
+          newTraining <- data.frame(new = currentCol)
+        }
+        colnames(newTraining) <- colN
+        count = count + 1
+    }
+  }
+}
+intrain <- createDataPartition(newTraining$classe , p= 0.6,list = T)
+trainSet <- newTraining[intrain[[1]],]
+testSet <- newTraining[-intrain[[1]],]
+newTesting <- testing[colnames(newTraining)[1:(dim(newTraining)[2] - 1)]]
+mod_1  = train(classe ~ . , mathod = 'rf',data = trainSet)
+pred_1 = predict(mod_1,testSet)
+mod_2  = train(classe ~ . , mathod = 'glm',data = trainSet)
+pred_2 = predict(mod_2,testSet)
+mod_3  = train(classe ~ . , mathod = 'gam',data = trainSet)
+pred_3 = predict(mod_3,testSet)
+all_pred <- data.frame(pred1 = pred_1,pred2 = pred_2,pred3 = pred_3)
+pred_All = predict(all_pred,newTesting)
