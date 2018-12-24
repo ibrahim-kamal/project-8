7817b279b7dcbb36b894895847f0e22a166d694e
ibrahim <ibrahimka2018@gmail.com>
2018-12-24 06:22:33 +0200

    first commit

--- project.R
+++ project.R
@@ -46,3 +46,26 @@ mod_3  = train(classe ~ . , mathod = 'gam',data = trainSet)
 pred_3 = predict(mod_3,testSet)
 all_pred <- data.frame(pred1 = pred_1,pred2 = pred_2,pred3 = pred_3)
 pred_All = predict(all_pred,newTesting)
+
+# get rf accuracy
+acc_1 <- (table(as.character(pred_1) == as.character(testSet$classe))[2]/length(as.character(pred_1)))[[1]]
+# get glm accuracy
+acc_2 <- (table(as.character(pred_2) == as.character(testSet$classe))[2]/length(as.character(pred_1)))[[1]]
+# get gam accuracy
+acc_3 <- (table(as.character(pred_3) == as.character(testSet$classe))[2]/length(as.character(pred_1)))[[1]]
+# get combine classification accuracy
+acc_All <- (table(as.character(pred_All) == as.character(testSet$classe))[2]/length(as.character(pred_1)))[[1]]
+
+#select the big accuracy and use it to predict class
+if(acc_1 > acc_2 && acc_1 > acc_3 && acc_1 > acc_All){
+  predict(mod_1,testing)
+}
+elseif(acc_2 > acc_3 && acc_2 > acc_All){
+  predict(mod_2,testing)
+}
+else if(acc_3 > acc_All){
+  predict(mod_3,testing)
+}
+else{
+  predict(mod_All,testing)
+}
